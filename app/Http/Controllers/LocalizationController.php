7c08b466e6dbfc4005973bb6c0de4d6ef761098f
Rovshen Annayev <81748081+rovshena@users.noreply.github.com>
2022-01-29 22:18:41 +0500

    Added Localization controller

---
+++ app/Http/Controllers/LocalizationController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\App;
+
+class LocalizationController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param $locale
+     * @return RedirectResponse
+     */
+    public function __invoke($locale)
+    {
+        if (Arr::exists(config('app.available_locales', ['en']), $locale)) {
+            App::setLocale($locale);
+            session()->put('locale', $locale);
+        }
+        return redirect()->back();
+    }
+}
