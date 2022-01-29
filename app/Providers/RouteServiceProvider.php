94169a01f6de43a3e3aaf39ac338ca80c8ad61b5
Rovshen Annayev <81748081+rovshena@users.noreply.github.com>
2022-01-29 17:33:16 +0500

    Initialized Service Providers

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/home';
+    public const HOME = '/admin';
 
     /**
      * The controller namespace for the application.
@@ -58,6 +58,10 @@ class RouteServiceProvider extends ServiceProvider
     {
         RateLimiter::for('api', function (Request $request) {
             return Limit::perMinute(60)->by(optional($request->user())->id ?: $request->ip());
+        });
+
+        RateLimiter::for('contact', function (Request $request){
+            return Limit::perMinute(5)->by($request->ip());
         });
     }
 }
